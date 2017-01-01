4c9850bf36febffc3c8e050e460a9a8b79a910ac
JackLiu <61819905@qq.com>
2017-01-01 15:23:30 +0800

    1

--- include/functions.php
+++ include/functions.php
@@ -3271,13 +3271,13 @@ function get_username($id, $big = false, $link = true, $bold = true, $target = f
 		$username = ($link == true ? "<a ". $link_ext . " href=\"userdetails.php?id=" . $id . "\"" . ($target == true ? " target=\"_blank\"" : "") . " class='". get_user_class_name($arr['class'],true) . "_Name'>" . $username . "</a>" : $username) . $pics . ($withtitle == true ? " (" . ($arr['title'] == "" ?  get_user_class_name($arr['class'],false,true,true) : "<span class='".get_user_class_name($arr['class'],true) . "_Name'><b>".htmlspecialchars($arr['title'])) . "</b></span>)" : "");
 
 		<!-- $username = "<span class=\'nowrap\'>" . ( $bracket == true ? "(" . $username . ")" : $username) . "</span>"; -->
-		$username = $bracket == true ? "(" . $username . ")" : $username;
+		$username = ( $bracket == true ? "(" . $username . ")" : $username);
 	}
 	else
 	{
 		$username = "<i>".$lang_functions['text_orphaned']."</i>";
 		<!-- $username = "<span class=\'nowrap\'>" . ( $bracket == true ? "(" . $username . ")" : $username) . "</span>"; -->
-		$username = $bracket == true ? "(" . $username . ")" : $username);
+		$username = ( $bracket == true ? "(" . $username . ")" : $username) ;
 	}
 	if (func_num_args() == 1) { //One argument=is default display of username, save it in static array
 		$usernameArray[$id] = $username;
