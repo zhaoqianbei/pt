f31381aed7d8322b679b200a1742d23744d1c471
JackLiu <61819905@qq.com>
2020-02-10 21:17:44 +0800

    教程

--- details.php
+++ details.php
@@ -64,8 +64,8 @@ else {
 		$js = '<script type="text/javascript">var $config = {
 		    url: window.location.href,
 		    source: "找前辈网 - 7500学编程，手把手教到就业满意！就业没有5000元/月，少多少退多少！", 
-		    title: "'.htmlspecialchars($row["name"]).'", 
-		    description: "类型：'.$row["cat_name"].'，上传者：'.$zuozhe.'('.htmlspecialchars(trim($row["small_descr"])).')，文件大小：'.mksize($row["size"]).'", 
+		    title: "'.htmlspecialchars($row["name"]).'\n'.htmlspecialchars(trim($row["small_descr"])).'", 
+		    description: "类型：'.$row["cat_name"].'，上传者：'.$zuozhe.'，文件大小：'.mksize($row["size"]).'", 
 		    image: "https://pt.24di.cn/ico.png", 
 		    sites: ["wechat", "qq", "qzone", "weibo"], 
 		    disabled: ["google", "facebook", "twitter"], 
